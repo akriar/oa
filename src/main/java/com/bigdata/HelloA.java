c7252bfdf29cae6604cb74f2499b1b27a25f2ffa
Your Name <you@example.com>
2020-04-16 19:12:28 +0800

    冲突代码提交

--- src/main/java/com/bigdata/HelloA.java
+++ src/main/java/com/bigdata/HelloA.java
@@ -2,7 +2,9 @@ package com.bigdata;
 
 public class HelloA {
 	public static void main(String[] args) {
-		System.out.println("今天的工作完成了，什么都不相干了");
-		System.out.println("你代码是错的");
+		System.out.println("aaaaaaaa");
+		System.out.println("bbbbbbbbb");
+		System.out.println("aaaaaaaaaa");
+		System.out.println("ccccccccccc");
 	}
 }
