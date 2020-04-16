5493eb1b40045f05bdf075713027d27117044f51
Your Name <you@example.com>
2020-04-16 18:03:54 +0800

    小黄你下班

--- src/main/java/com/bigdata/HelloA.java
+++ src/main/java/com/bigdata/HelloA.java
@@ -3,7 +3,6 @@ package com.bigdata;
 public class HelloA {
 	public static void main(String[] args) {
 		System.out.println("今天的工作完成了，什么都不相干了");
-		System.out.println("我下班了----小明");
-		System.out.println("我下班了----小黄 ");
+		System.out.println("小黄你还想下班");
 	}
 }
