ff86abc5064891d5d65459e3129c95df6fd92662
Your Name <you@example.com>
2020-04-16 19:06:24 +0800

    第一次提交

--- src/main/java/com/bigdata/HelloA.java
+++ src/main/java/com/bigdata/HelloA.java
@@ -2,7 +2,7 @@ package com.bigdata;
 
 public class HelloA {
 	public static void main(String[] args) {
-		System.out.println("今天的工作完成了，什么都不相干了");
-		System.out.println("小黄你还想下班");
+		System.out.println("aaaaaaaaaa");
+		System.out.println("ccccccccccc");
 	}
 }
